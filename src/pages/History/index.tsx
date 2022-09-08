2b0a95e704379af608243ab76b9023af7dd1585b
Caroline Vieira <caroline@fetch.ly>
2022-09-08 19:13:52 +0200

    td status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,56 @@
+import { HistoryContaner, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContaner>
+      <h1>My history</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task name</td>
+              <td>10 minutes</td>
+              <td>2 days ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task name</td>
+              <td>5 minutes</td>
+              <td>10 days ago</td>
+              <td>
+                <Status statusColor="red">Cancelled</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task name</td>
+              <td>60 minutes</td>
+              <td>1 days ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task name</td>
+              <td>60 minutes</td>
+              <td>1 days ago</td>
+              <td>
+                <Status statusColor="yellow">in process</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContaner>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -86,6 +86,9 @@ export const StartCoundownBtn = styled.button`
   border: 0;
   padding: 1rem;
   border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   gap: 0.5rem;
   font-weight: bold;
